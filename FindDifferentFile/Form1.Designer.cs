85f95ef4ad0deac0b211e1aae38a426108387dcb
RyuGiKen <576074134@qq.com>
2022-09-05 12:26:43 +0800

    实现找出两目录不同文件。

--- FindDifferentFile/Form1.Designer.cs
+++ FindDifferentFile/Form1.Designer.cs
@@ -32,7 +32,15 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -41,8 +49,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "导入";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -56,9 +65,9 @@
             this.panel1.Controls.Add(this.listBox1);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(12, 44);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(378, 437);
+            this.panel1.Size = new System.Drawing.Size(378, 405);
             this.panel1.TabIndex = 7;
             // 
             // listBox1
@@ -70,14 +79,87 @@
             this.listBox1.ItemHeight = 12;
             this.listBox1.Location = new System.Drawing.Point(3, 32);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(372, 400);
+            this.listBox1.Size = new System.Drawing.Size(372, 364);
             this.listBox1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listBox2);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Location = new System.Drawing.Point(396, 44);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(378, 405);
+            this.panel2.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(3, 32);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(372, 364);
+            this.listBox2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "导入";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "找出差异";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(145, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "找出相同";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(271, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "清空";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(800, 500);
@@ -87,6 +169,8 @@
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -97,6 +181,13 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- FindDifferentFile/Form1.cs
+++ FindDifferentFile/Form1.cs
@@ -2,19 +2,151 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RyuGiKen;
+using static System.Net.WebRequestMethods;
 
 namespace FindDifferentFile
 {
     public partial class Form1 : Form
     {
+        string path1;
+        string path2;
+        List<FileInfo> files1 = new List<FileInfo>();
+        List<FileInfo> files2 = new List<FileInfo>();
         public Form1()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// 读取文件
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static List<FileInfo> LoadFiles(string path, ListBox listBox = null)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                return null;
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            List<FileInfo> result = GetFile.GetFileInfos(path);
+            Console.WriteLine("找到" + result?.Count);
+            UpdateList(listBox, result.ToArray());
+            return result;
+        }
+        /// <summary>
+        /// 更新列表
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="listBox"></param>
+        /// <param name="array"></param>
+        static void UpdateList<T>(ListBox listBox, T[] array) where T : class
+        {
+            if (listBox != null)
+            {
+                listBox.Items.Clear();
+                listBox.Items.AddRange(array);
+            }
+        }
+        /// <summary>
+        /// 导入目录1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+                return;
+            path1 = textBox1.Text;
+            files1 = LoadFiles(path1, listBox1);
+        }
+        /// <summary>
+        /// 导入目录2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox2.Text))
+                return;
+            path2 = textBox2.Text;
+            files2 = LoadFiles(path2, listBox2);
+        }
+        /// <summary>
+        /// 找出差异
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            if (files1 == null || files1.Count < 1 || files2 == null || files2.Count < 1)
+                return;
+            for (int i = files1.Count - 1; i >= 0; i--)
+            {
+                for (int j = files2.Count - 1; j >= 0; j--)
+                {
+                    if (files1[i].Name.ToLower() == files2[j].Name.ToLower())
+                    {
+                        files1.RemoveAt(i);
+                        files2.RemoveAt(j);
+                        break;
+                    }
+                }
+            }
+            UpdateList(listBox1, files1.ToArray());
+            UpdateList(listBox2, files2.ToArray());
+            Console.WriteLine("找到" + (files1.Count + files2.Count) + "，耗时 " + (DateTime.Now - dateTime).TotalMilliseconds.ToString() + " ms");
+        }
+        /// <summary>
+        /// 找出相同
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            if (files1 == null || files1.Count < 1 || files2 == null || files2.Count < 1)
+                return;
+            List<FileInfo> data1 = new List<FileInfo>();
+            List<FileInfo> data2 = new List<FileInfo>();
+            for (int i = files1.Count - 1; i >= 0; i--)
+            {
+                for (int j = files2.Count - 1; j >= 0; j--)
+                {
+                    if (files1[i].Name.ToLower() == files2[j].Name.ToLower())
+                    {
+                        data1.Add(files1[i]);
+                        files1.RemoveAt(i);
+                        data2.Add(files2[j]);
+                        files2.RemoveAt(j);
+                        break;
+                    }
+                }
+            }
+            files1 = data1;
+            files2 = data2;
+            UpdateList(listBox1, files1.ToArray());
+            UpdateList(listBox2, files2.ToArray());
+            Console.WriteLine("找到" + (files1.Count + files2.Count) + "，耗时 " + (DateTime.Now - dateTime).TotalMilliseconds.ToString() + " ms");
+        }
+        /// <summary>
+        /// 清空按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            files1?.Clear();
+            files2?.Clear();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
         }
     }
 }
